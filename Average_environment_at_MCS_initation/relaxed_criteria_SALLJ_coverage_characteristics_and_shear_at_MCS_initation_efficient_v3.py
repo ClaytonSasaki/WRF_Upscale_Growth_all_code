ba2ec2e701bd09859298e350d6a99aa7a22ebcd0
Clayton Sasaki <crs326@atmos.washington.edu>
2024-09-20 17:10:28 -0700

    Updated file so it read in MCS tracks correctly and get the WRF file at each time corresponding to a MCS that initiated within the chosen region

--- Average_environment_at_MCS_initation/relaxed_criteria_SALLJ_coverage_characteristics_and_shear_at_MCS_initation_efficient_v3.py
+++ Average_environment_at_MCS_initation/relaxed_criteria_SALLJ_coverage_characteristics_and_shear_at_MCS_initation_efficient_v3.py
@@ -18,7 +18,7 @@ based on 'relaxed_criteria_SALLJ_spatial_areas_coverage_efficient_v3.py'
 import matplotlib
 matplotlib.use('Agg') 
 
-from datetime import timedelta
+from datetime import datetime
 import csv
 from netCDF4 import Dataset
 import matplotlib.pyplot as plt
@@ -42,35 +42,70 @@ from wrf import (to_np, getvar, smooth2d, get_cartopy, cartopy_xlim,
 MCS_tracks_file_path = '/home/disk/monsoon/relampago/analysis//mcs_tracks/wrf/stats/mcs_tracks_pf_20181015_20190430.nc'
 MCS_tracks_ncfile = Dataset(MCS_tracks_file_path,'r')
 
-# get MCS initation times and centroid locations
-datetime_str = getvar(MCS_tracks_ncfile, 'datetimestring')
-MCS_center_lons = getvar(MCS_tracks_ncfile, 'pf_lon')
-MCS_center_lats = getvar(MCS_tracks_ncfile, 'pf_lat')
+## get MCS initation times and centroid locations
+#datetime_str = getvar(MCS_tracks_ncfile, 'datetimestring', meta=False)
+#MCS_center_lons = getvar(MCS_tracks_ncfile, 'pf_lon', meta=False)
+#MCS_center_lats = getvar(MCS_tracks_ncfile, 'pf_lat', meta=False)
 
-MCS_time_initation_str = datetime_str[:,0,0]
-MCS_center_lons_initation = MCS_center_lon[:,0,0]
-MCS_center_lats_initation = MCS_center_lat[:,0,0]
+# gets MCS times in byte format
+MCS_datetime_bytes = MCS_tracks_ncfile.variables['datetimestring']
 
-print(MCS_initation_times_str)
+# gets MCS iniation times still in byte format
+MCS_datetime_initiation_bytes = MCS_datetime_bytes[:,0,:]
+
+# concatenates the bytes that make up one MCS initation time into a single byte string and decodes to a string
+MCS_datetime_initiation_str = np.array([bytes(bytes_for_one_time).decode('UTF-8') for bytes_for_one_time in MCS_datetime_initiation_bytes])
+
+# another way to do the same as the line above with the join function instead
+#print([b''.join(row).decode('UTF-8') for row in datetime_bytes])
+
+# gets the MCS center lons and lats (nmaxpf chosen to be 0, NOTE: not sure what nmaxpf is?????????)
+MCS_center_lons = MCS_tracks_ncfile.variables['pf_lon'][:,:,0]
+MCS_center_lats = MCS_tracks_ncfile.variables['pf_lat'][:,:,0]
+
+# get MCS initation center lons and lats
+MCS_center_lons_initiation = MCS_center_lons[:,0]
+MCS_center_lats_initiation = MCS_center_lats[:,0]
+
+#print(MCS_time_initation_str)
+#print(MCS_center_lons_initation)
+#print(MCS_center_lats_initation)
+print('dates of MCS init \n', MCS_datetime_initiation_str)
+print('lons of MCS init \n', MCS_center_lons_initiation)
+print('lats of MCS init \n', MCS_center_lats_initiation)
+# close the MCS tracks file
+MCS_tracks_ncfile.close()
 
 # save MCS initation times and centroid locations of those within defined box near the SDC
 lon_min = -66.25
 lon_max = -61.1
-lat_min = -29.5
-lat_max = -34.0
+lat_min = -34.0
+lat_max = -29.5
 
 filtered_indices = []
 
-for index, (lat, lon) in enumerate(zip(MCS_time_initation_str, MCS_center_lats_initation, MCS_center_lons_initation):
+for index, (lat, lon) in enumerate(zip( MCS_center_lats_initiation, MCS_center_lons_initiation)):
+    #print('lat', lat)
+    #print('lon', lon)
+    #print(lat_min <= lat <= lat_max)
+    #print(lon_min <= lon <= lon_max)
     if lat_min <= lat <= lat_max and lon_min <= lon <= lon_max:
+        #print('yes')
         filtered_indices.append(index)
+    else:
+        pass
+        #print('no')
                                    
-MCS_time_initation_filtered_str = MCS_time_initation_str[filtered_indices]
-MCS_center_lons_initation_filtered = MCS_center_lons_initation[filtered_indices]
-MCS_center_lats_initation_filtered = MCS_center_lats_initation[filtered_indices]
+MCS_datetime_initiation_filtered_str = MCS_datetime_initiation_str[filtered_indices]
+MCS_center_lons_initiation_filtered = MCS_center_lons_initiation[filtered_indices]
+MCS_center_lats_initiation_filtered = MCS_center_lats_initiation[filtered_indices]
+
+# number of MCSs within chosen region
+num_MCS_init_region = len(MCS_datetime_initiation_filtered_str)
+print('# of MCSs within chosen region: ', num_MCS_init_region)
 
 # go through list of times/centroids
-for MCS_time, MCS_center_lon, MCS_center_lat in zip(MCS_time_initation_filtered_str, MCS_center_lons_initation_filtered, MCS_center_lats_initation_filtered):
+for MCS_datetime, MCS_center_lon, MCS_center_lat in zip(MCS_datetime_initiation_filtered_str, MCS_center_lons_initiation_filtered, MCS_center_lats_initiation_filtered):
 
     # get lats/lons of region based on centroid
     lat_bottom_left = MCS_center_lat - 0.75 
@@ -82,14 +117,18 @@ for MCS_time, MCS_center_lon, MCS_center_lat in zip(MCS_time_initation_filtered_
     # get the file necessary
     path_wrf = '/home/disk/monsoon/relampago/raw/wrf/'                  
                                    
-    MCS_date_str = MCS_time.date().strftime('%Y%m%d')
+    MCS_datetime_dt = datetime.strptime(MCS_datetime, '%Y-%m-%d_%H:%M:')
+    
+    MCS_datetime_str = MCS_datetime_dt.strftime('%Y%m%d')
 
-    wrf_file_name = '/wrfout_d01_%s-%s-%s_%s:00:00' %(SALLJ_time.strftime('%Y'), SALLJ_time.strftime('%m'), SALLJ_time.strftime('%d'), SALLJ_time.strftime('%H'))
+    wrf_file_name = '/wrfout_d01_%s-%s-%s_%s:00:00' %(MCS_datetime_dt.strftime('%Y'), MCS_datetime_dt.strftime('%m'), MCS_datetime_dt.strftime('%d'), MCS_datetime_dt.strftime('%H'))
 
-    wrf_file_path = path_wrf + dt_date_str + wrf_file_name
+    wrf_file_path = path_wrf + MCS_datetime_str + wrf_file_name
+    
+    print('paths of MCS within region \n', wrf_file_path)
 
     # get the netCDF
-    wrf_ncfile = Dataset(file_path,'r')
+    wrf_ncfile = Dataset(wrf_file_path,'r')
     
     # find if SALLJ is present and if so calculate characteristics
     
@@ -98,5 +137,7 @@ for MCS_time, MCS_center_lon, MCS_center_lat in zip(MCS_time_initation_filtered_
     # calculate Richardson number
     
     # save MCS characteristics (time, centroid, growth rate, cloud sheild area) and environmental characteristics (SALLJ characterisitcs, shear) to netCDF or dateframe file
+    
+    wrf_ncfile.close()
 
     
